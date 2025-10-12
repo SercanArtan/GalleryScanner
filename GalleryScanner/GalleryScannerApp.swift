be5f6e05df9da4ad5090f5ed15ee9a2e717cbcfc
Sercan Artan <sercan.artan@std.bogazici.edu.tr>
2025-10-12 23:33:15 +0300

    Initial Commit

---
+++ GalleryScanner/GalleryScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GalleryScannerApp.swift
+//  GalleryScanner
+//
+//  Created by SERCAN ARTAN on 12.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct GalleryScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
